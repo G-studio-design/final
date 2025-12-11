64d24d96c1a681dd4dd7d4cf4b70bbfc9ca0f56a
I wayan Govina <iwayangovina@gmail.com>
2025-12-11 03:40:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-import tailwindConfig from './tailwind.config.js';
-
-const config = {
-  plugins: [tailwindcss(tailwindConfig), autoprefixer],
-};
-
-export default config;
