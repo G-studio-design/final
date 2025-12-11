76ad80314952c3890271a30e0b855a4f3f931b61
I wayan Govina <iwayangovina@gmail.com>
2025-12-11 03:11:25 +0000

    masalah error webpack ini sepertinya sebelum nya pernah terjadi deh dan

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,9 @@
-/** @type {import('postcss-load-config').Config} */
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import tailwindConfig from './tailwind.config.js';
+
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+  plugins: [tailwindcss(tailwindConfig), autoprefixer],
 };
 
 export default config;
